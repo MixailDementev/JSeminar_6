8185fbe51a8b62b3f68e2af83729d1aa7d39004a
MDementev <vanneste1207@gmail.com>
2023-01-08 18:56:42 +0300

    домашнее задание 6

---
+++ catalog.java
@@ -0,0 +1,8 @@
+package JSeminar_6;
+
+public class catalog extends laptop {
+
+    public catalog(int id, int ram, int ssd, String os, String color, String brand) {
+        super(id, ram, ssd, os, color, brand);
+    }
+}
